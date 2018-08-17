5dc1cacaffa311d3dc741c398e1facd24606242d
Jose P Valdes <jose.valdes@dzne.de>
2018-08-17 11:19:10 +0200

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -24,4 +24,12 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     download_url=DOWNLOAD_URL,
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Science/Research',
+    ],
 )
